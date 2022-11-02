7fe85d14be3333b44b9900ab9bfe38e2f0a8e1fd
JonathanHdb <j.wider@stud.uni-heidelberg.de>
2022-11-02 11:42:48 +0100

    timestep extraction funciton
    Writer function that can extract the true month and year from each dataset. This is necessary because the files each have different calendars and time units

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import os.path
+import numpy as np
 
 
 def flatten(l):
@@ -52,6 +53,45 @@ def create_hash_from_description(description):
         else:
             res_dict[key] = value
     return str(hex(hash(frozenset(res_dict.items()))))
+
+
+def get_years_months(t, units, calendar):
+    """
+    The data sets come with different time units and calendars.
+    We write a function that returns the 'true' time steps.
+    @param t: Timesteps to be converted
+    @param units: String containing a description of the time units used. Can be extracted via netCDF4.
+    @param calendar: String containing a description of the calendar used. Can be extracted via netCDF4.
+    @return: 
+    """
+    if units == "months since 801-1-15 00:00:00":
+        years = 801 + t // 12
+        months = 0 + t % 12
+    elif units == "month as %Y%m.%f":
+        timesteps = [t_.split(".")[0] for t_ in t.astype(str)]
+        years = [int(t_[:-2]) for t_ in timesteps]
+        months = [int(t_[-2:])-1 for t_ in timesteps]  # want months to start with zero instead of one
+    elif units == "day as %Y%m%d.%f":
+        timesteps = [t_.split(".")[0] for t_ in t.astype(str)]
+        years = [int(t_[:-4]) for t_ in timesteps]
+        months = [int(t_[-4:-2])-1 for t_ in timesteps]  # want months to start with zero instead of one
+    elif units == "months since 850-1-15 00:00:00":
+        years = 850 + t // 12
+        months = 0 + t % 12
+    elif units == "days since 0850-01-01 00:00:00":
+        if calendar == "365_day":
+            years = 850 + t // 365
+            ds = [31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 0]
+            months = [np.argmin(np.abs(ds - t_ % 365)) for t_ in t]
+    elif units == "days since 2350-12-01 00:00:00":
+        if calendar == "360_day":
+            y_ref = 654
+            m_ref = 11
+            years = - y_ref + 850 + (t // 360)  # in the dataset the year (2954) corresponds to year 850
+            months = (t+m_ref*30) % 360 // 30
+    elif units == "days since 0000-01-01 00:00:00":
+        return None, None
+    return years, months
 
 
 def get_year_mon_day_from_timesteps(time_steps, ref_date):
