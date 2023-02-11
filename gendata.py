4ad2c85e180d765126644542225fe3d260300641
JonathanHdb <j.wider@stud.uni-heidelberg.de>
2023-02-11 17:01:55 +0100

    added license, added statement in gendata.py

--- gendata.py
+++ gendata.py
@@ -1,4 +1,29 @@
-"""Module to generate the spherical mnist data set - taken from S2CNN paper and modified"""
+"""
+Module to generate the spherical mnist data set - taken from 'https://github.com/jonkhler/s2cnn/blob/master/examples/mnist/gendata.py' and modified. 
+Licence of original code:
+
+MIT License
+
+Copyright (c) 2017 Taco Cohen, Mario Geiger, Jonas Köhler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 
 import gzip
 import pickle
